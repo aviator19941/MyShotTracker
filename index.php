ec5771f3d61da99c5363c2f769db7014affcc7a5
Avinash Sharma <aviator1994@gmail.com>
2017-04-09 18:22:14 -0700

    My first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+</head>
+
+
+
+<body>
+
+<div class="test"> 
+  <form action="welcome.php" method="post">
+    Name: <input type="text" name="name"><br>
+    Email: <input type="text" name="email"><br>
+    <input type="submit">
+  </form>
+</div>
+
+</body>
+</html>
